44c60126c1386d151cf364e194ef5ffe2edf603e
Nuansa Lembayung <nuansa@icloud.com>
2017-07-29 21:43:44 +0800

    manual segue

--- app-swoosh/app-swoosh/Contoller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Contoller/WelcomeVC.swift
@@ -15,7 +15,7 @@ class WelcomeVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func unwindFromSckillVC(unwindSegue: UIStoryboardSegue) {
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
 
