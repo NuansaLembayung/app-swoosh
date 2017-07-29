ddb0c305cbaab10bf506635cbe9b9e52316667d2
Nuansa Lembayung <nuansa@icloud.com>
2017-07-29 22:22:04 +0800

    data

--- app-swoosh/app-swoosh/Contoller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Contoller/LeagueVC.swift
@@ -38,6 +38,12 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
